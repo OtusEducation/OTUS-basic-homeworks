52ed4d942e43b98424184c08ce91878831317251
Tagir Murtazin <tr.murtazin@yandex.ru>
2023-11-21 13:57:07 +0300

    Домашнее задание 15.

---
+++ src/main/java/ru/murtazin/homeworks/homework15/Employee.java
@@ -0,0 +1,19 @@
+package ru.murtazin.homeworks.homework15;
+
+public class Employee {
+    private final String name;
+    private final int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
